b3f51330269e566cac260ec8a0e9b0a26ac4e742
uvsq-21405271 <camaraabdellahi@gmail.com>
2016-10-27 01:25:24 +0200

    the second test_2 test

--- fr.uvsq.coo.Groupe2.ex2_2/src/test/java/fr/uvsq/coo/BowlingGameTest.java
+++ fr.uvsq.coo.Groupe2.ex2_2/src/test/java/fr/uvsq/coo/BowlingGameTest.java
@@ -10,9 +10,12 @@ public class BowlingGameTest extends TestCase{
 	public void testGutterGame() throws Exception {
 		int n = 20;
 		int pins = 0;
-		for (int i = 0; i < n; i++)
-			g.roll(pins);
+		rollMany(n, pins);
 		assertEquals(0, g.score());
+		}
+	private void rollMany(int n, int pins) {
+		for (int i = 0; i < n; i++)
+		g.roll(pins);
 		}
 	public void testAllOnes() throws Exception {
 		Game g = new Game();
