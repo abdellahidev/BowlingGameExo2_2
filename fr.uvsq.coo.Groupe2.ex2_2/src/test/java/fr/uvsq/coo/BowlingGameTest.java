7032cc6f668e8fe5937765aedefafa4b1f2d7b38
uvsq-21405271 <camaraabdellahi@gmail.com>
2016-10-27 01:20:16 +0200

    le 2eme  test

--- fr.uvsq.coo.Groupe2.ex2_2/src/test/java/fr/uvsq/coo/BowlingGameTest.java
+++ fr.uvsq.coo.Groupe2.ex2_2/src/test/java/fr/uvsq/coo/BowlingGameTest.java
@@ -3,10 +3,15 @@ package fr.uvsq.coo;
 import junit.framework.TestCase;
 
 public class BowlingGameTest extends TestCase{
+	private Game g;
+	protected void setUp() throws Exception {
+		g = new Game();
+		}
 	public void testGutterGame() throws Exception {
-		Game g = new Game();
-		for (int i=0; i<20; i++)
-			g.roll(0);
+		int n = 20;
+		int pins = 0;
+		for (int i = 0; i < n; i++)
+			g.roll(pins);
 		assertEquals(0, g.score());
 		}
 	public void testAllOnes() throws Exception {
