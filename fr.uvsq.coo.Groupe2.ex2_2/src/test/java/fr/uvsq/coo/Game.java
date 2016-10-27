51525ec236bb31e9c372877e2dae62753017c32d
uvsq-21405271 <camaraabdellahi@gmail.com>
2016-10-27 09:26:51 +0200

    The Third  test_5

--- fr.uvsq.coo.Groupe2.ex2_2/src/test/java/fr/uvsq/coo/Game.java
+++ fr.uvsq.coo.Groupe2.ex2_2/src/test/java/fr/uvsq/coo/Game.java
@@ -1,17 +1,15 @@
 package fr.uvsq.coo;
 
 public class Game {
-	private int score=0;
 	private int rolls[] = new int[21];
 	private int currentRoll = 0;
 	public void roll(int pins) {
-		score+=pins;
 		rolls[currentRoll++] = pins;
 	}
 	public int score() {
 		int score = 0;
 		for (int i = 0; i < rolls.length; i++)
-		score += rolls[i];
+				score += rolls[i];
 		return score;
 		}
 }
